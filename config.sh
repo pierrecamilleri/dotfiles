424792e975ff9bd165e102d2af7c1a83b0f28c58
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2021-11-05 13:57:25 +0100

    First attempt at config script

---
+++ config.sh
@@ -0,0 +1,30 @@
+cd ~/dotfiles
+
+# Install nvim & fzf
+apt-get install nvim
+apt-get install fzf
+# nvim: install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+stow nvim
+# Install nvim plugins
+vim +PluginInstall +qall
+
+# Install shell colorscheme
+stow bash
+git clone https://github.com/aaron-williamson/base16-gnome-terminal.git ~/.config/base16-gnome-terminal
+~/.config/base16-gnome-terminal/color-scripts/base16-gruvbox-dark-soft.sh
+
+# Install ipython
+apt-get install ipython3
+stow ipython
+
+#sudo Install tmux
+apt-get install tmux
+apt-get install tmuxinator
+stow tmux
+stow tmuxinator
+
+
+# R and Radian
+pip3 install radian
+stow radian
